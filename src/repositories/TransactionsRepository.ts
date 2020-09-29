e325a290eaae2336ed28c21bc77dfacf6342431e
tito <titopalcorrea@gmail.com>
2020-09-28 21:00:48 -0300

    desafio-05

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -30,7 +30,7 @@ class TransactionsRepository {
             accumulator.income += transaction.value;
             break;
           case 'outcome':
-            accumulator.total += transaction.value;
+            accumulator.outcome += transaction.value;
             break;
           default:
             break;
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -22,7 +22,7 @@ class CreateTransactionService {
     const { total } = this.transactionsRepository.getBalance();
 
     if (type === 'outcome' && total < value) {
-      throw new Error('You donot have enough balance');
+      throw new Error('You do not have enough balance');
     }
 
     const transaction = this.transactionsRepository.create({
